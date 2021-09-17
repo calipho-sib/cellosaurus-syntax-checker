0f1851041422e829122e3aa30a64da45026b9270
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2021-09-17 23:25:02 +0200

    added script to easily get datat needed by parser

---
+++ get_shared_data.sh
@@ -0,0 +1,5 @@
+scp npteam@kant://share/sib/calipho/calipho/cellosaurus/cellosaurus.txt ./target/
+scp npteam@kant://share/sib/calipho/calipho/cellosaurus/cellosaurus_refs.txt ./target/
+scp npteam@kant://share/sib/calipho/calipho/cellosaurus/cellosaurus_xrefs.txt ./target/
+scp npteam@kant://share/sib/calipho/calipho/cellosaurus/celloparser.cv ./target/
+
