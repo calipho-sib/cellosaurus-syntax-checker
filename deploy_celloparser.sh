51e57373c88080b52e4bff69061f998205e33e61
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2022-01-21 16:16:05 +0100

    added xsd in celloparser repository

---
+++ deploy_celloparser.sh
@@ -0,0 +1,3 @@
+scp target/cello-parser-0.1-jar-with-dependencies.jar npteam@kant:/share/sib/calipho/calipho/cellosaurus/celloparser.jar
+scp src/main/scala/org/nextprot/parser/cellosaurus/CelloParser.scala npteam@kant:/share/sib/calipho/calipho/cellosaurus/sources
+
