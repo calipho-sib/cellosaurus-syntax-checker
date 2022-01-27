bfb346e8eefccc43438f366c96959f60887bc284
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2022-01-27 14:19:33 +0100

    no comment

--- get_shared_data.sh
+++ get_shared_data.sh
@@ -2,4 +2,6 @@ scp npteam@kant://share/sib/calipho/calipho/cellosaurus/cellosaurus.txt ./target
 scp npteam@kant://share/sib/calipho/calipho/cellosaurus/cellosaurus_refs.txt ./target/
 scp npteam@kant://share/sib/calipho/calipho/cellosaurus/cellosaurus_xrefs.txt ./target/
 scp npteam@kant://share/sib/calipho/calipho/cellosaurus/celloparser.cv ./target/
-
+scp npteam@kant://share/sib/calipho/calipho/cellosaurus/cellosaurus.xsd ./target/
+ ./target/
+ 
