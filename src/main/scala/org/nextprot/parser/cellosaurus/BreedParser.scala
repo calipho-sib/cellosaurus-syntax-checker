1de8a858e08c9067a89602a81cb0c66d22cc55c3
pmichel <pamichel@infomaniak.ch>
2024-05-28 11:53:55 +0200

    adapted breed parser to newly supported xref databases RS and FlyBase_Strain

--- src/main/scala/org/nextprot/parser/cellosaurus/BreedParser.scala
+++ src/main/scala/org/nextprot/parser/cellosaurus/BreedParser.scala
@@ -5,26 +5,30 @@ import scala.collection.mutable.Map
 import scala.xml._
 
 
-class Breed(val name: String, val vbo: String) {
+class Breed(val name: String, val xref_str: String) {
 
-  var xrefs = List[DbXref]()
+  var xref_list = List[DbXref]()
   init
 
   def init = {
-    if (vbo != null && vbo.length > 0) {
+    if (xref_str != null && xref_str.length > 0) {
+      // split db and acs
+      val xref_split = xref_str.split("=")
+      val db : String = xref_split(0)
+      val acs_str : String = xref_split(1)
       // use split on char rather on string (would be double escaped "\\+")
-      val acs = vbo.substring(4).split('+') 
+      val acs = acs_str.split('+')
       val labels = name.split(" x ") 
       var idx = 0
       acs.foreach(ac => {
         val pos = labels(idx).indexOf("(")
         val label = 
           if (pos == -1) labels(idx) else labels(idx).substring(0,pos).trim()
-        val xref = new DbXref("VBO", ac, label = label)
-        xrefs = xref :: xrefs
+        val xref = new DbXref(db, ac, label = label)
+        xref_list = xref :: xref_list
         idx += 1
       })
-      xrefs = xrefs.reverse
+      xref_list = xref_list.reverse
     }
   }
 
@@ -32,9 +36,9 @@ class Breed(val name: String, val vbo: String) {
     <breed>
       {name}
       {
-      if (xrefs.size > 0) 
+      if (xref_list.size > 0) 
         <xref-list>
-        {xrefs.map(_.toXML)}
+        {xref_list.map(_.toXML)}
         </xref-list>
       else
         Null
@@ -46,21 +50,30 @@ object BreedParser {
 
   def parseLine(rawline: String): Breed = {
 
+    // Data examples: 
+    // Eker x Long-Evans; RS=RS:0000385+RS:0000347.
+    // Hereford x Limousin; VBO=VBO_0000232+VBO_0000274.
+    // Oregon-R; FlyBase_Strain=FBsn0000276.
+
+    val validDbSet = Set("VBO", "RS", "FlyBase_Strain")
+
     // remove final '.' if necessary
     var line = rawline
     if (rawline.endsWith(".")) line = rawline.substring(0,rawline.length-1) 
 
-    // split line elements on ";"
+    // split line elements on ";" 
     val elems = line.split("; ")
     val name: String = elems(0).trim()
-    var vbo : String = null
+    var xref_str : String = null
     if (elems.size ==2) {
-      vbo = elems(1)
-      if (!vbo.startsWith("VBO=")) throw new Exception("Invalid breed xref database")
+      xref_str = elems(1)
+      val xref_split = xref_str.split("=")
+      val db = xref_split(0)
+      if ( ! validDbSet.contains(db)) throw new Exception("Invalid breed xref database")
     } else if (elems.size>2) {
       throw new Exception("Too many fields separated with ';'")
     }
-    return new Breed(name, vbo)
+    return new Breed(name, xref_str)
   }
 
   def main(args: Array[String]): Unit = {
